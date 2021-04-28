6599997901569b856b975923de2cf395b87bf881
Azhar Mobeen <amobeen@accelaero.com>
2021-04-28 23:35:15 +0400

    swagger integrated

--- src/test/java/com/az/SpringBootWithH2ApplicationTests.java
+++ src/test/java/com/az/SpringBootWithH2ApplicationTests.java
@@ -1,16 +1,13 @@
 package com.az;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+
 public class SpringBootWithH2ApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
 	}
 
 }
